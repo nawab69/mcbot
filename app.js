88d9b2789e2581bbd6006fe25cd7407a3fa0f3d2
Nawab69 <44573643+nawab69@users.noreply.github.com>
2019-02-08 17:08:13 +0600

    Var to const

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 const Discord = require('discord.js');              // require disord.js node module
 const client = new Discord.Client();                
 const Request = require("request");                 // require request node module
-console.log('mcBOT Connected Successfully');        // Bot connected message show in terminal
-
 client.on("message", message => {
 
   // run if the message start with https://steemit.com
   
 if(message.content.startsWith("https://steemit.com")){
-var msg = message.content;               // store input message
-var url = "http://api.steemtools.cf/get_blog_count?url=";       // steemtools api
-var api = url.concat(msg);                                    // generate api address
+const msg = message.content;               // store input message
+const url = "http://api.steemtools.cf/get_blog_count?url=";       // steemtools api
+const api = url.concat(msg);                                    // generate api address
 
  Request.get(api, (error, response, body) => { if(error) { return console.dir(error);
  // Get data in JSON from API by HTTP request
   }
  
-var result = JSON.parse(body);        // convert json to object
+const result = JSON.parse(body);        // convert json to object
 
 //run only when total_words value is not equal to zero or null
 
@@ -56,9 +54,6 @@ message.channel.send({embed:{
   // Here you can add more command
 
 });
-
-
-
+console.log('mcBOT Connected Successfully');        // Bot connected message show in terminal
 // client.login("client secret");     // insert client secret here (uncomment)
-
 client.login(process.env.BOT_TOKEN);  //BOT_TOKEN is the Client Secret. Only for heroku server.  (comment this line if you run own server)
